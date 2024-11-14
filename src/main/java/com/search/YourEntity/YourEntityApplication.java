000c337a482ee3c2926d63e725db4d9d6fd7e23e
Ruchika Ambat <ruchikaambat11gmail.com>
2024-11-15 01:45:56 +0530

    comites

---
+++ src/main/java/com/search/YourEntity/YourEntityApplication.java
@@ -0,0 +1,13 @@
+package com.search.YourEntity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourEntityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/search/YourEntity/YourEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search.YourEntity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
